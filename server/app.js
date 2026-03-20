85da541c6cb5996e64f9b44e0dc73c8d2c345bcc
PIPALIYAJAYDIP <pipaliyaj3@gmail.com>
2026-03-20 23:13:34 +0530

    package setup done

---
+++ server/app.js
@@ -0,0 +1,26 @@
+/*
+
+"dependencies": {
+    "express": "^5.2.1",
+    "dotenv": "^16.5.0",
+    "jsonwebtoken": "^9.0.2",
+    "cookie-parser": "^1.4.7",
+    "uuid": "^11.1.0",
+    "http-proxy-middleware": "^3.0.0",
+    "axios": "^1.6.0",
+    "express-rate-limit": "^7.0.0",
+    "rate-limit-redis": "^3.0.0",
+    "ioredis": "^5.3.2",
+    "helmet": "^7.0.0",
+    "cors": "^2.8.5",
+    "morgan": "^1.10.0",
+    "opossum": "^8.0.0"
+  },
+  "devDependencies": {
+    "nodemon": "^3.1.9",
+    "eslint": "^9.37.0",
+    "eslint-plugin-n": "^17.23.1",
+    "cross-env": "^7.0.3"
+  }
+
+*/
