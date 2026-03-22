6bc69e42b02aa9f6bdb6ec587283c3184025826b
PIPALIYAJAYDIP <pipaliyaj3@gmail.com>
2026-03-22 19:05:56 +0530

    Redis configiration done

---
+++ server/config/redis.client.js
@@ -0,0 +1,20 @@
+import Redis from 'ioredis';
+import { REDIS_HOST, REDIS_PORT, REDIS_PASSWORD, REDIS_DB } from './env.js'
+
+const redis = new Redis({
+    host: REDIS_HOST,
+    port: REDIS_PORT,
+    password: REDIS_PASSWORD,
+    db: REDIS_DB,
+    retryStrategy: (times) => Math.min(times * 50, 2000),
+});
+
+redis.on('error', (err) => {
+    console.error('Redis connection error: ', err);
+});
+
+redis.on('connect', () => {
+    console.log('Redis connected successfully.')
+});
+
+export default redis;
