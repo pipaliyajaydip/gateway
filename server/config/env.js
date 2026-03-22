7df16533fc543c0b4974d56a21f0b599e1aca085
PIPALIYAJAYDIP <pipaliyaj3@gmail.com>
2026-03-22 19:06:45 +0530

    env.js updated with redis env

--- server/config/env.js
+++ server/config/env.js
@@ -14,6 +14,12 @@ dotenv.config({
 });
 
 export const PORT = process.env.PORT;
+
+export const REDIS_HOST = process.env.REDIS_HOST;
+export const REDIS_PORT = process.env.REDIS_PORT;
+export const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
+export const REDIS_DB = process.env.REDIS_DB;
+
 export const USER_SERVICE = process.env.USER_SERVICE;
 
 export const JWT_SECRET = process.env.JWT_SECRET;
