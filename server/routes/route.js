da7d03b157decee16f896a9b753dfc12446c3eb5
PIPALIYAJAYDIP <pipaliyaj3@gmail.com>
2026-06-14 23:51:26 +0530

    Routes on proxyReq, Res and error logs added

--- server/routes/route.js
+++ server/routes/route.js
@@ -5,12 +5,58 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const router = express.Router();
 
-router.use('/health', healthRouter);
-router.use('/users', createProxyMiddleware({
+// router.use('/health', healthRouter);
+
+// router.use('/users', createProxyMiddleware({
+//     target: USER_SERVICE,
+//     changeOrigin: true,
+//     pathRewrite: {
+//         '^/users': '/users'
+//     },
+//     logLevel: 'debug',
+// }));
+
+router.use('/health', createProxyMiddleware({
     target: USER_SERVICE,
     changeOrigin: true,
-    pathRewrite: {
-        '^/users': '/users'
+    on: {
+      proxyReq: (proxyReq, req) => {
+        console.log('ProxyReq:', req.method, req.originalUrl);
+      },
+
+      proxyRes: (proxyRes, req) => {
+        console.log('ProxyRes:', proxyRes.statusCode);
+      },
+
+      error: (err, req, res) => {
+        console.error('Proxy Error:', err.message);
+      }
+    },
+    logLevel: 'debug',
+}));
+
+router.use('/api/auth', (req, res, next) => {
+  console.log('Auth proxy route matched');
+  next();
+});
+
+console.log("USER_SERVICE: ", USER_SERVICE);
+
+router.use('/auth', createProxyMiddleware({
+    target: USER_SERVICE,
+    changeOrigin: true,
+    on: {
+        proxyReq: (proxyReq, req) => {
+          console.log('ProxyReq:', req.method, req.originalUrl);
+        },
+
+        proxyRes: (proxyRes, req) => {
+          console.log('ProxyRes:', proxyRes.statusCode);
+        },
+
+        error: (err, req, res) => {
+          console.error('Proxy Error:', err.message);
+        }
     },
     logLevel: 'debug',
 }));
